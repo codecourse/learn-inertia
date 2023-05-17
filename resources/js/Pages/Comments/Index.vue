e40012a8435d2d36b4718a27133b240c4391153d
Alex Garrett-Smith <alex@codecourse.com>
2023-05-17 11:28:57 +0100

    Episode 07

--- resources/js/Pages/Comments/Index.vue
+++ resources/js/Pages/Comments/Index.vue
@@ -1,9 +1,13 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
 
 defineProps({
     posts: Array
+})
+
+const form = useForm({
+    body: '',
 })
 </script>
 
@@ -15,6 +19,13 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-3">
+                <form class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                    {{ form }}
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="5" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" v-model="form.body"></textarea>
+                    <button type="submit" class="mt-2 bg-gray-700 px-4 py-2 rounded-md font-medium text-white">Post</button>
+                </form>
+
                 <div v-for="post in posts" :key="post.id">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900">
