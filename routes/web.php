2c9b8c7e64b01aacd4de89906b261b90f75261b1
Alex Garrett-Smith <alex@codecourse.com>
2023-05-17 13:46:20 +0100

    Episode 23

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::post('/comments', CommentStoreController::class)->name('comments.store');
 
 Route::get('/users/{user}', UserShowController::class)->name('users.show');
 
+Route::inertia('/about', 'About', ['appName' => config('app.name')]);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
